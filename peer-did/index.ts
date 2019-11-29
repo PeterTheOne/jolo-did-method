4c2125019f071f3611517c380b2ec5286a1c2423
Charles Cunningham <c.a.cunningham6@gmail.com>
2019-11-29 14:38:51 +0100

    trying to do resolver/exchange stuff

--- peer-did/index.ts
+++ peer-did/index.ts
@@ -52,3 +52,104 @@ console.log(getDID(genesisDDO({
     controller: '#id',
     publicKeyBase58: 'keylmao'
 })))
+
+type Delta = {
+    change: string,
+    by: {
+        key: string,
+        sig: string
+    }[],
+    when: string
+}
+
+type DidDoc = {
+    id: string,
+    publicKey: GenerationInput[],
+    authorization: {
+        profiles: [],
+        rules: [
+            {
+                // this DSL is called SGL: https://evernym.github.io/sgl/
+                grant: string[],
+                when: {id: string },
+                id: string
+            }
+        ]
+    }
+}
+
+interface IResolver {
+    update: (did: string, ddo: DidDoc) => boolean,
+    resolve: (did: string) => string
+}
+
+interface Transport<M> {
+    send: (message: M) => Promise<boolean>
+}
+
+class Resolver implements IResolver {
+    private perspective: Map<string, DidDoc>
+
+    constructor() {
+        this.perspective = new Map()
+    }
+
+    public update(did: string, ddo: DidDoc) {
+        this.perspective.set(did, ddo)
+    }
+
+    public resolve(did: string) {
+        return this.perspective.get(did)
+    }
+}
+
+const DidExchangeInviter = <M extends DidDoc>(
+    resolver: IResolver,
+    transport: Transport<M>,
+    validate: (m: M) => boolean
+) => function*(
+) {
+    // null state
+
+    // send invitation
+    const invitation: M = yield
+    transport.send(invitation)
+
+    // invited state
+
+    // recieve exchange_request
+    const response: M = yield
+
+        // requested state
+
+    if (validate(response)) {
+        resolver.update(response.id, response)
+        resolver.resolve(response.id) //??
+        // done when??
+    }
+
+}
+
+const DidExchangeInvitee = <M extends DidDoc>(
+    resolver: IResolver,
+    transport: Transport<M>
+) => function*(
+
+) {
+    // null state
+    // recieve invitation
+    const invitation: M = yield
+
+    // invited state
+    // send exchange_request
+    const request: M = yield
+
+    transport.send(request)
+
+    // requested state
+    // recieve exchange_response
+    const response: M = yield
+
+    // responded state
+    // do yo thing
+}
