ea3dd9e86226b4fbfe05d41aab0f168a1d38c039
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-07-17 17:47:50 +0200

    test(new): update tests to new interface

--- local-did-resolver/tests/index.test.ts
+++ local-did-resolver/tests/index.test.ts
@@ -1,13 +1,19 @@
-import { getResolver, getRegistrar, extractMessageId } from "../ts";
+import { getResolver, getRegistrar } from "../ts";
 import { Resolver } from "did-resolver";
 import { testDid, testDidDoc } from "./test.data";
 import { createDb } from "../ts/db";
+
+import { validateEvents, getIdFromEvent, getIcp } from '@jolocom/native-utils-node'
 
 describe("Local DID Resolver", () => {
   describe("getResolver", () => {
     it("It should fail to resolve an unknown local DID", async () => {
       const testDb = createDb()
-      const resolver = new Resolver(getResolver(testDb));
+      const resolver = new Resolver(getResolver({
+        dbInstance: testDb,
+        validateEvents
+      }));
+
 
       return expect(resolver.resolve(testDid)).rejects.toEqual(
         new Error('resolver returned null for did:un:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
@@ -16,10 +22,20 @@ describe("Local DID Resolver", () => {
 
     it('It should correctly register a known local DID', async () => {
       const testDb = createDb()
-      const keyEvent = await getRegistrar(testDb).create()
-      const { icp } = JSON.parse(keyEvent)
-      testDb.append([icp])
-      return expect(new Resolver(getResolver(testDb)).resolve(extractMessageId(icp))).resolves.toEqual({})
+      const keyEvent = await getRegistrar({
+        dbInstance: testDb,
+        validateEvents,
+        getIdFromEvent,
+        create: getIcp,
+      }).create({}) as string
+      const icp: string = JSON.parse(keyEvent).icp
+      console.log(icp)
+      testDb.append(getIdFromEvent(icp), [icp])
+      return expect(new Resolver(getResolver({
+        dbInstance: testDb,
+        validateEvents
+      })).resolve(getIdFromEvent(keyEvent)))
+        .resolves.toEqual(await validateEvents(JSON.stringify([icp])))
     });
   });
 });
